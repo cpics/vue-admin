5edaad234f82eb44a695a4c19225d5764f4e626a
沈飞 <shenfei@jiangduoduo.com>
2019-07-13 17:32:40 +0800

    [*]首页添加图表

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -16,19 +16,21 @@
       <div class="data-statistics">
         <div class="statistics-col">
           <div class="statistics-tit">代码标准表统计</div>
-          <div class="statistics-chart" />
+          <div class="statistics-chart">
+            <div ref="dmChart" style="height:90%;width:90%;" />
+          </div>
         </div>
         <div class="statistics-col">
           <div class="statistics-tit">代码数据量统计</div>
-          <div class="statistics-chart" />
+          <div ref="liangChart" class="statistics-chart" style="height:90%;width:90%;" />
         </div>
         <div class="statistics-col">
           <div class="statistics-tit">主数据标准统计</div>
-          <div class="statistics-chart" />
+          <div ref="lChart" class="statistics-chart" style="height:90%;width:90%;" />
         </div>
         <div class="statistics-col">
           <div class="statistics-tit">主数据字段统计</div>
-          <div class="statistics-chart" />
+          <div ref="zhuChart" class="statistics-chart" style="height:90%;width:90%;" />
         </div>
       </div>
     </div>
@@ -36,13 +38,55 @@
 </template>
 
 <script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
 import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
   data() {
     return {
-
+      dmChart: null,
+      liangChart: null,
+      lChart: null,
+      zhuChart: null,
+      dmOptions: {
+        color: ['#3398DB'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '直接访问',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+          }
+        ]
+      }
     }
   },
   computed: {
@@ -50,79 +94,96 @@ export default {
       'roles'
     ])
   },
-  created() {
+  mounted() {
+    this.initDmChart()
+  },
+  methods: {
+    initDmChart() {
+      console.log(this.$refs.dmChart)
+      this.dmChart = echarts.init(this.$refs.dmChart)
+      this.dmChart.setOption(this.dmOptions)
+
+      this.liangChart = echarts.init(this.$refs.liangChart)
+      this.liangChart.setOption(this.dmOptions)
+
+      this.lChart = echarts.init(this.$refs.lChart)
+      this.lChart.setOption(this.dmOptions)
+
+      this.zhuChart = echarts.init(this.$refs.zhuChart)
+      this.zhuChart.setOption(this.dmOptions)
+    }
 
   }
 }
 </script>
 <style scoped="scoped" lang="scss">
-  .dashboard-tit{
+.dashboard-tit {
     color: #333;
     font-size: 24px;
     font-weight: normal;
     margin: 0;
     padding: 10px 0;
-  }
-  .dashboard-data{
+}
+.dashboard-data {
     font-size: 0;
     padding: 20px 0;
     margin-left: -20px;
     max-width: 1000px;
-    .dashboard-col{
-      width: 300px;
-      height: 160px;
-      color: #fff;
-      font-size: 18px;
-      text-align: center;
-      line-height: 160px;
-      display: inline-block;
-      margin: 0 0 20px 20px;
-      word-break: keep-all;
-      white-space: nowrap;
-      overflow: hidden;
-      border-radius: 5px;
-      background-color: #5d95ff;
-      background: linear-gradient(to right,#6bb5ff,#5d95ff);
-      &:nth-child(2){
-        background-color: #f08d36;
-        background: linear-gradient(to right,#fab83b,#f08d36);
-      }
-      &:nth-child(3){
-        background-color: #2f9fc9;
-        background: linear-gradient(to right,#48c5bf,#2f9fc9);
-      }
-      &:nth-child(4){
-        background-color: #8f60f2;
-        background: linear-gradient(to right,#ad71e9,#8f60f2);
-      }
-      &:nth-child(5){
-        background-color: #019b65;
-        background: linear-gradient(to right,#0edd94,#019b65);
-      }
-      &:nth-child(6){
-        background-color: #e44c3e;
-        background: linear-gradient(to right,#ec6955,#e44c3e);
-      }
+    .dashboard-col {
+        width: 300px;
+        height: 160px;
+        color: #fff;
+        font-size: 18px;
+        text-align: center;
+        line-height: 160px;
+        display: inline-block;
+        margin: 0 0 20px 20px;
+        word-break: keep-all;
+        white-space: nowrap;
+        overflow: hidden;
+        border-radius: 5px;
+        background-color: #5d95ff;
+        background: linear-gradient(to right, #6bb5ff, #5d95ff);
+        &:nth-child(2) {
+            background-color: #f08d36;
+            background: linear-gradient(to right, #fab83b, #f08d36);
+        }
+        &:nth-child(3) {
+            background-color: #2f9fc9;
+            background: linear-gradient(to right, #48c5bf, #2f9fc9);
+        }
+        &:nth-child(4) {
+            background-color: #8f60f2;
+            background: linear-gradient(to right, #ad71e9, #8f60f2);
+        }
+        &:nth-child(5) {
+            background-color: #019b65;
+            background: linear-gradient(to right, #0edd94, #019b65);
+        }
+        &:nth-child(6) {
+            background-color: #e44c3e;
+            background: linear-gradient(to right, #ec6955, #e44c3e);
+        }
     }
-  }
-  .data-statistics{
+}
+.data-statistics {
     font-size: 0;
     padding: 20px 0;
     margin-left: -20px;
-    .statistics-col{
-      width: 450px;
-      color: #333;
-      font-size: 16px;
-      display: inline-block;
-      margin-left: 20px;
-      .statistics-tit{
-        padding: 0 0 10px;
-      }
-      .statistics-chart{
+    .statistics-col {
         width: 450px;
-        height: 280px;
-        margin-bottom: 15px;
-      }
+        color: #333;
+        font-size: 16px;
+        display: inline-block;
+        margin-left: 20px;
+        .statistics-tit {
+            padding: 0 0 10px;
+        }
+        .statistics-chart {
+            width: 450px;
+            height: 280px;
+            margin-bottom: 15px;
+        }
     }
-  }
+}
 </style>
