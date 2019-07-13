3fc21369d715fa1da5695aae60d385d30254fc07
dongshan <dongshan@jiangduoduo.com>
2019-07-13 20:13:25 +0800

    [+]首页

--- src/views/test/temList/temList.vue
+++ src/views/test/temList/temList.vue
@@ -1,15 +1,146 @@
 <template>
-  <div>
-    质量模板配置
+  <div class="g-container">
+    <div class="tem-handle-btn">
+      <el-button type="primary">添加</el-button>
+      <el-button type="danger">删除</el-button>
+    </div>
+    <el-table
+      ref="multipleTable"
+      :data="tableData"
+      style="width: 100%"
+    >
+      <el-table-column
+        type="selection"
+        width="55"
+      />
+      <el-table-column
+        prop="template"
+        label="规则模板"
+      />
+      <el-table-column
+        prop="type"
+        label="规则类型"
+      />
+      <el-table-column
+        prop="useType"
+        label="使用类型"
+      />
+      <el-table-column
+        prop="email"
+        label="邮件提醒"
+      >
+        <template>
+          <el-switch
+            v-model="value1"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            active-text="YES"
+            inactive-text="NO"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100"
+      >
+        <template>
+          <el-button type="text" size="small" @click="dialogFormVisible = true">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog title="维护规则模板" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="规则类型" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择">
+            <el-option label="唯一性" value="shanghai" />
+            <el-option label="完整性" value="beijing" />
+            <el-option label="确定性" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="使用类型" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择">
+            <el-option label="单字段" value="shanghai" />
+            <el-option label="多字段" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="规则模板名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="模板内容" :label-width="formLabelWidth">
+          <el-input v-model="form.content" type="textarea" />
+        </el-form-item>
+        <el-form-item label="描述" :label-width="formLabelWidth">
+          <el-input v-model="form.desc" type="textarea" />
+        </el-form-item>
+        <el-form-item label="参数名1" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="参数名2" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="参数名3" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="参数名4" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">保 存</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      value1: true,
+      tableData: [{
+        template: '不在代码表',
+        type: '唯一性',
+        useType: '单字段'
+      }, {
+        template: '不在代码表',
+        type: '唯一性',
+        useType: '单字段'
+      }, {
+        template: '不在代码表',
+        type: '唯一性',
+        useType: '单字段'
+      }, {
+        template: '不在代码表',
+        type: '唯一性',
+        useType: '单字段'
+      }, {
+        template: '不在代码表',
+        type: '唯一性',
+        useType: '单字段'
+      }],
+      multipleSelection: [],
+      dialogFormVisible: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        content: '',
+        desc: ''
+      },
+      formLabelWidth: '120px'
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped="scoped" lang="scss">
+.tem-handle-btn{
+  padding-bottom: 30px;
+}
 </style>
--- src/views/test/temRule/temRule.vue
+++ src/views/test/temRule/temRule.vue
@@ -1,15 +1,266 @@
 <template>
-  <div>
-    质量规则配置
+  <div class="g-container">
+    <div class="tem-rule-main">
+      <div class="tem-rule-tree">
+        <el-input
+          v-model="filterText"
+          placeholder="数据目录"
+        />
+        <el-tree
+          ref="tree"
+          class="filter-tree"
+          :data="data"
+          :props="defaultProps"
+          default-expand-all
+          :filter-node-method="filterNode"
+        />
+      </div>
+      <div class="tem-rule-content">
+        <div class="tem-rule-filter">
+          <el-form :model="form">
+            <el-form-item label="规则模板" :label-width="formLabelWidth">
+              <el-select v-model="form.template" placeholder="请选择">
+                <el-option label="不符合身份证号规则" value="shanghai" />
+                <el-option label="不唯一" value="beijing" />
+              </el-select>
+            </el-form-item>
+          </el-form>
+          <div class="tem-button">
+            <el-button type="primary">查询</el-button>
+            <el-button type="success">添加</el-button>
+            <el-button type="danger">删除</el-button>
+          </div>
+        </div>
+        <el-table
+          ref="multipleTable"
+          :data="tableData"
+          tooltip-effect="dark"
+          style="width: 100%"
+          @selection-change="handleSelectionChange"
+        >
+          <el-table-column
+            type="selection"
+            width="55"
+          />
+          <el-table-column
+            prop="object"
+            label="检测对象"
+          />
+          <el-table-column
+            prop="field"
+            label="检测字段"
+          />
+          <el-table-column
+            prop="rule"
+            label="检测规则"
+          />
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="200"
+          >
+            <template>
+              <el-button type="text" size="small" @click="dialogFormVisible = true">修改</el-button>
+              <el-button type="text" size="small">调试</el-button>
+              <el-button type="text" size="small">显示设置</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+    <el-dialog title="编辑T_JZG检测规则" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="规则模板" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择">
+            <el-option label="不符合身份证号规则" value="shanghai" />
+            <el-option label="不唯一" value="beijing" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="检测数据范围" :label-width="formLabelWidth">
+          <el-input v-model="form.content" type="textarea" />
+        </el-form-item>
+      </el-form>
+      <div class="dialog-footer form-button">
+        <el-button type="primary" @click="dialogFormVisible = false">保 存</el-button>
+      </div>
+      <el-table
+        ref="multipleTable"
+        :data="tableData"
+        height="300"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column
+          type="selection"
+          width="55"
+        />
+        <el-table-column
+          prop="object"
+          label="检测字段名"
+        />
+        <el-table-column
+          prop="field"
+          label="检测字段中文名"
+        />
+        <el-table-column
+          prop="rule"
+          label="说明"
+        />
+      </el-table>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
 
+  data() {
+    return {
+      filterText: '',
+      form: {
+        template: '',
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        content: '',
+        desc: ''
+      },
+      formLabelWidth: '120px',
+      tableData: [{
+        object: 'T_JZG[教职工基本信息]',
+        field: 'ZGH[职工号]',
+        rule: '不唯一'
+      }, {
+        object: 'T_JZG[教职工基本信息]',
+        field: 'ZGH[身份证号]',
+        rule: '不符合身份证号规则'
+      }, {
+        object: 'T_JZG[教职工基本信息]',
+        field: 'ZGH[职工号]',
+        rule: '不唯一'
+      }, {
+        object: 'T_JZG[教职工基本信息]',
+        field: 'ZGH[职工号]',
+        rule: '不唯一'
+      }, {
+        object: 'T_JZG[教职工基本信息]',
+        field: 'ZGH[身份证号]',
+        rule: '不符合身份证号规则'
+      }, {
+        object: 'T_JZG[教职工基本信息]',
+        field: 'ZGH[身份证号]',
+        rule: '不符合身份证号规则'
+      }],
+      dialogFormVisible: false,
+      data: [{
+        id: 1,
+        label: '一级 1',
+        children: [{
+          id: 4,
+          label: '二级 1-1',
+          children: [{
+            id: 9,
+            label: '三级 1-1-1'
+          }, {
+            id: 10,
+            label: '三级 1-1-2'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: '一级 2',
+        children: [{
+          id: 5,
+          label: '二级 2-1'
+        }, {
+          id: 6,
+          label: '二级 2-2'
+        }]
+      }, {
+        id: 3,
+        label: '一级 3',
+        children: [{
+          id: 7,
+          label: '二级 3-1'
+        }, {
+          id: 8,
+          label: '二级 3-2'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val)
+    }
+  },
+
+  methods: {
+    filterNode(value, data) {
+      if (!value) return true
+      return data.label.indexOf(value) !== -1
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped="scoped" lang="scss">
+.tem-rule-main{
+  &:after{
+    content: "";
+    font-size: 0;
+    display: block;
+    height: 0;
+    clear: both;
+    visibility: hidden;
+  }
+  .tem-rule-tree{
+    width: 300px;
+    float: left;
+    padding: 20px;
+    min-height: 380px;
+    border: 1px solid #dcdfe6;
+    border-radius: 5px;
+    .el-input{
+      margin-bottom: 10px;
+    }
+  }
+  .tem-rule-content{
+    margin-left: 340px;
+    padding: 20px;
+    min-height: 380px;
+    border: 1px solid #dcdfe6;
+    border-radius: 5px;
+  }
+  .tem-rule-filter{
+    height: 40px;
+    margin-bottom: 20px;
+    &:after{
+      content: "";
+      font-size: 0;
+      display: block;
+      height: 0;
+      clear: both;
+      visibility: hidden;
+    }
+    .el-form{
+      float: left;
+      width: 400px;
+      .el-form-item{
+        margin-bottom: 0;
+      }
+    }
+    .tem-button{
+      float: right;
+    }
+  }
+}
 </style>
