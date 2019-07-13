5518fa85e2639d6bc50b30e001c30540a8880d36
Zhu Jun <zhujun@jiangduoduo.com>
2019-07-13 22:06:36 +0800

    [+]主数据管理

--- src/views/mainData/index/index.vue
+++ src/views/mainData/index/index.vue
@@ -1,15 +1,153 @@
 <template>
-  <div>
-    主数据概要
+  <div class="g-container">
+    <div class="dashboard-box">
+      <h2 class="dashboard-tit">概况</h2>
+      <div class="dashboard-data">
+        <div class="dashboard-col">总数据数：450</div>
+        <div class="dashboard-col">集成数据：300</div>
+        <div class="dashboard-col">数据资产：600万+</div>
+      </div>
+    </div>
+    <div class="dashboard-box">
+      <h2 class="dashboard-tit">数据量TOP10</h2>
+      <div class="data-statistics">
+        <div class="statistics-col">
+          <div class="statistics-chart">
+            <div ref="dmChart" style="height:90%;width:90%;" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
 export default {
-
+  name: 'MainData',
+  data() {
+    return {
+      dmChart: null,
+      dmOptions: {
+        color: ['#3398DB'],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '直接访问',
+            type: 'bar',
+            barWidth: '60%',
+            data: [10, 52, 200, 334, 390, 330, 220]
+          }
+        ]
+      }
+    }
+  },
+  mounted() {
+    this.initDmChart()
+  },
+  methods: {
+    initDmChart() {
+      this.dmChart = echarts.init(this.$refs.dmChart)
+      this.dmChart.setOption(this.dmOptions)
+    }
+  }
 }
 </script>
-
-<style>
-
+<style scoped="scoped" lang="scss">
+.dashboard-data {
+  padding: 20px 0;
+  margin-left: -20px;
+  &:after {
+    content: '';
+    font-size: 0;
+    display: block;
+    height: 0;
+    clear: both;
+    visibility: hidden;
+  }
+  .dashboard-col {
+    width: 30%;
+    min-width: 280px;
+    height: 160px;
+    color: #fff;
+    font-size: 18px;
+    text-align: center;
+    line-height: 160px;
+    float: left;
+    margin: 0 0 20px 20px;
+    word-break: keep-all;
+    white-space: nowrap;
+    overflow: hidden;
+    border-radius: 5px;
+    background-color: #5d95ff;
+    background: linear-gradient(to right, #6bb5ff, #5d95ff);
+    &:nth-child(2) {
+      background-color: #f08d36;
+      background: linear-gradient(to right, #fab83b, #f08d36);
+    }
+    &:nth-child(3) {
+      background-color: #2f9fc9;
+      background: linear-gradient(to right, #48c5bf, #2f9fc9);
+    }
+    &:nth-child(4) {
+      background-color: #8f60f2;
+      background: linear-gradient(to right, #ad71e9, #8f60f2);
+    }
+    &:nth-child(5) {
+      background-color: #019b65;
+      background: linear-gradient(to right, #0edd94, #019b65);
+    }
+    &:nth-child(6) {
+      background-color: #e44c3e;
+      background: linear-gradient(to right, #ec6955, #e44c3e);
+    }
+  }
+}
+.data-statistics {
+  font-size: 0;
+  padding: 20px 0;
+  margin-left: -20px;
+  .statistics-col {
+    width: 100%;
+    min-width: 450px;
+    color: #333;
+    font-size: 16px;
+    display: inline-block;
+    margin-left: 20px;
+    .statistics-tit {
+      padding: 0 0 10px;
+    }
+    .statistics-chart {
+      width: 100%;
+      height: 280px;
+      margin-bottom: 15px;
+    }
+  }
+}
 </style>
--- src/views/mainData/manage/manage.vue
+++ src/views/mainData/manage/manage.vue
@@ -1,15 +1,168 @@
 <template>
-  <div>
-    主数据管理
+  <div class="g-container">
+    <div class="double-columns tem-rule-main">
+      <div class="column-left">
+        <div class="border-box tem-rule-tree">
+          <el-input v-model="filterText" placeholder="按表名查询" />
+          <el-tree ref="tree" class="filter-tree" :data="data" :props="defaultProps" default-expand-all :filter-node-method="filterNode" />
+        </div>
+      </div>
+      <div class="column-right">
+        <div class="border-box">
+          <div class="tem-rule-filter" style="text-align:center">
+            <h4>主数据总体情况汇总（截止2019-06-21）</h4>
+          </div>
+          <el-table ref="multipleTable" :data="tableData" tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table-column prop="id" label="ID" />
+            <el-table-column prop="tableCname" label="表中文名称" />
+            <el-table-column prop="tableName" label="表名" />
+            <el-table-column prop="count" label="数据记录数" />
+            <el-table-column prop="wordCount" label="字段数" />
+          </el-table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
 
+  data() {
+    return {
+      filterText: '',
+      form: {
+        template: '',
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        content: '',
+        desc: ''
+      },
+      tableData: [{
+        id: '1',
+        tableCname: '学生课表数据',
+        tableName: 'T_JW_XSKB',
+        count: '12345',
+        wordCount: '10'
+      },
+      {
+        id: '2',
+        tableCname: '学生课表数据',
+        tableName: 'T_JW_XSKB',
+        count: '12345',
+        wordCount: '10'
+      },
+      {
+        id: '3',
+        tableCname: '学生课表数据',
+        tableName: 'T_JW_XSKB',
+        count: '12345',
+        wordCount: '10'
+      },
+      {
+        id: '4',
+        tableCname: '学生课表数据',
+        tableName: 'T_JW_XSKB',
+        count: '12345',
+        wordCount: '10'
+      },
+      {
+        id: '5',
+        tableCname: '学生课表数据',
+        tableName: 'T_JW_XSKB',
+        count: '12345',
+        wordCount: '10'
+      }
+      ],
+      data: [{
+        id: 1,
+        label: '一级 1',
+        children: [{
+          id: 4,
+          label: '二级 1-1',
+          children: [{
+            id: 9,
+            label: '三级 1-1-1'
+          }, {
+            id: 10,
+            label: '三级 1-1-2'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: '一级 2',
+        children: [{
+          id: 5,
+          label: '二级 2-1'
+        }, {
+          id: 6,
+          label: '二级 2-2'
+        }]
+      }, {
+        id: 3,
+        label: '一级 3',
+        children: [{
+          id: 7,
+          label: '二级 3-1'
+        }, {
+          id: 8,
+          label: '二级 3-2'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val)
+    }
+  },
+
+  methods: {
+    filterNode(value, data) {
+      if (!value) return true
+      return data.label.indexOf(value) !== -1
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped="scoped" lang="scss">
+.tem-rule-main {
+  .tem-rule-tree {
+    .el-input {
+      margin-bottom: 10px;
+    }
+  }
+  .tem-rule-filter {
+    height: 40px;
+    margin-bottom: 20px;
+    &:after {
+      content: '';
+      font-size: 0;
+      display: block;
+      height: 0;
+      clear: both;
+      visibility: hidden;
+    }
+    .el-form {
+      float: left;
+      width: 400px;
+      .el-form-item {
+        margin-bottom: 0;
+      }
+    }
+    .tem-button {
+      float: right;
+    }
+  }
+}
 </style>
