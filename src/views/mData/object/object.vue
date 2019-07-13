72ce0b933c31046a001521751955ba8674c45fa3
victorwang0526 <fengxuanwang@qq.com>
2019-07-14 00:30:54 +0800

    t

--- src/views/mData/object/object.vue
+++ src/views/mData/object/object.vue
@@ -68,7 +68,7 @@
             <el-table-column
               label="操作"
             >
-              <template slot-scope="sope">
+              <template>
                 <el-button>修改</el-button>
                 <el-button>预览</el-button>
                 <el-button>生成</el-button>
