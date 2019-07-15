0f2f27737ce8817f6face3d06c5fe246f7650a72
shenfei <shenfei@moduoduodeMacBook-Pro.local>
2019-07-15 16:40:52 +0800

    test

--- src/components/BackToTop/index.vue
+++ src/components/BackToTop/index.vue
@@ -12,8 +12,7 @@ export default {
   props: {
     visibilityHeight: {
       type: Number,
-      default: 400,
-      hehe:111
+      default: 400
     },
     backPosition: {
       type: Number,
