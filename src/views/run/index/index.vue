a512ee30a099c2d23ea7dcd0b8a4db61f18992c3
沈飞 <shenfei@jiangduoduo.com>
2019-07-15 11:28:12 +0800

    运行监控管理

---
+++ src/views/run/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>数据集成管理</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/run/sql/sql.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>数据控监控</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
